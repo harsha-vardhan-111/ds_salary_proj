0479a27db9a61bf58e0c5110a4151805edc56dad
harsha vardhan <harshabk33@gmail.com>
2022-01-15 19:30:17 +0530

    '

---
+++ untitled1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 14 20:55:25 2022
+
+@author: Harsha Vardhan
+"""
+
+import glassdoor_scrapper as gs
+import pandas as pd
+
+path = 'C:/Users/Harsha Vardhan/projects/ds_salary_proj/chromedriver'
+df = gs.get_jobs('data scientist',15,False,path,10)
